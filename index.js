461e1e187e0c3e31343f47a6c48b3baa270e0f72
Ginner Zapata <gzapata@vizzn.ca>
2023-03-01 21:40:10 -0300

    basic routing

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("this is the overview");
+  }
+  if (pathName === "/product") {
+    res.end("this is the product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "txt/html",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
