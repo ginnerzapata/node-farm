7410e9fedef0d815d1c610fafa7f30fd79196102
Ginner Zapata <gzapata@vizzn.ca>
2023-03-01 21:23:24 -0300

    first server

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const fs = require("fs");
 const http = require("http");
 
-fs.readFile("./txt/start.txt", "utf8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("your file has been writen");
-      });
-    });
-  });
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listeing to resquests on port 8000");
 });
