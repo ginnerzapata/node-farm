1722e84fef37165205eec1bf7f21d5b9cb6a9eb2
Ginner Zapata <gzapata@vizzn.ca>
2023-03-01 23:23:21 -0300

    final part of the project

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("this is the overview");
-  }
-  if (pathName === "/product") {
-    res.end("this is the product");
-  } else if (pathName.includes("/api")) {
+  const { query, pathname } = url.parse(req.url, true);
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
-      "Content-type": "txt/html",
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
-    res.end("<h1>Page not found</h1>");
+    res.end("<h1>Page not found!</h1>");
   }
 });
 
